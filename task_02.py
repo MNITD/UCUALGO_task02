a70cf5bde58e312a65e695211599471da3c58373
MNITD <greatbogdansmith@gmail.com>
2016-10-02 21:48:57 +0300

    Insertion sort of inversions replaced by Merge sort

--- task_02.py
+++ task_02.py
@@ -19,13 +19,13 @@ def sort_matrix(matrix, x, row_len):
 #
 #     return target_set
 
-def merge(movie_set, begin, mid, end, sorted_set):
+def merge(movie_set, begin, mid, end, sorted_set, is_uno):
     n = begin
     m = mid
     inversions = 0
 
     while n < end:
-        if begin < mid and (m >= end or movie_set[begin] < movie_set[m]):
+        if begin < mid and (m >= end or ((is_uno and movie_set[begin] < movie_set[m]) or (not is_uno and movie_set[begin][1] < movie_set[m][1] ))):
             sorted_set[n] = movie_set[begin]
             begin += 1
         else:
@@ -37,14 +37,14 @@ def merge(movie_set, begin, mid, end, sorted_set):
     return inversions
 
 
-def merge_sort(user_set, begin, end, sorted_set):
+def merge_sort(user_set, begin, end, sorted_set, is_uno):
     if (end - begin) < 2:
         return 0
     inversions = 0
     mid = int((begin + end) / 2)
-    inversions += merge_sort(sorted_set, begin, mid, user_set)
-    inversions += merge_sort(sorted_set, mid, end, user_set)
-    inversions += merge(user_set, begin, mid, end, sorted_set)
+    inversions += merge_sort(sorted_set, begin, mid, user_set, is_uno)
+    inversions += merge_sort(sorted_set, mid, end, user_set, is_uno)
+    inversions += merge(user_set, begin, mid, end, sorted_set, is_uno)
 
     return inversions
 
@@ -55,18 +55,10 @@ def count_inversions(data, x):
     inversions = []
     while i < len(data):
         if i != x:
-            inversions.append([i, merge_sort(data[i].copy(), 0, len(data[i]), data[i].copy())])
+            inversions.append([i, merge_sort(data[i].copy(), 0, len(data[i]), data[i].copy(), True)])
         i += 1
 
-    i = 1
-    while i < len(inversions):
-        temp = inversions[i]
-        j = i
-        while j > 0 and inversions[j - 1][1] > temp[1]:
-            inversions[j] = inversions[j - 1]
-            j -= 1
-        inversions[j] = temp
-        i += 1
+    merge_sort(inversions.copy(), 0, len(inversions), inversions, False)
 
     return inversions
 
